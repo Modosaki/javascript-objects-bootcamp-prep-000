1f4cd76331ccfcdbebf483f7017c26f978bb7ae4
Karim Ezzat <modosaki@gmail.com>
2018-02-27 15:19:30 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = { artistName : "songTitle"};
 
 function updatePlaylist (playlist,artistName,songTitle){
-  return Object.asign({},obj, {[key]: value})
+  return Object.asign({},playlist, {[artistName]: songTitle})
   
 }
