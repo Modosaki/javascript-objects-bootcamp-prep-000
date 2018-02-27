475381492a197c91a17fcd0b649c55c209fb71c3
Karim Ezzat <modosaki@gmail.com>
2018-02-27 15:30:48 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -7,5 +7,6 @@ function updatePlaylist (playlist,artistName,songTitle){
 
 function removeFromPlaylist (playlist,artistName){
   delete playlist.artistName;
-  return Object.assign({},playlist, {[artistName]: songTitle})
+  return playlist
+
 }
